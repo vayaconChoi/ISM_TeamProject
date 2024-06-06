b27cabac4d8642dd305d885f47cfea946123ba1d
ParkGiyoun <cosil2b2@naver.com>
2024-06-07 08:32:21 +0900

    기윤 - 24-06-07 경매데이터 대쉬보드 업데이트 (AT 도매시장 Open API) 사과, 배 동시 출력 적용 및 데이터 출력 개선

--- dashboard/api/gonggong.py
+++ dashboard/api/gonggong.py
@@ -17,7 +17,13 @@ def get_live_auction():
     load_dotenv(dotenv_path=env_path)
     key = os.getenv('LIVE_AUCTION')
 
-    url = f"https://at.agromarket.kr/openApi/price/real.do?serviceKey={key}&apiType=json&pageNo=1&whsalCd=110001&largeCd=06&midCd=01"
-    response = requests.get(url)
+    apple_url = f"https://at.agromarket.kr/openApi/price/real.do?serviceKey={key}&apiType=json&pageNo=1&whsalCd=110001&largeCd=06&midCd=01"
+    response = requests.get(apple_url)
     data = response.json()['data']
+
+    pear_url = f"https://at.agromarket.kr/openApi/price/real.do?serviceKey={key}&apiType=json&pageNo=1&whsalCd=110001&largeCd=06&midCd=02"
+    response = requests.get(pear_url)
+    for i in response.json()['data']:
+        data.append(i)
+
     return data
